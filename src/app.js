02035a7c4991b5da190bf3af702780f31f22c1a1
Rodrigo Moraes <rodrigo.moraes001@mymdc.net>
2022-10-06 00:58:22 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  var domainGenerator = (pronoun, adj, noun) => {
+    for (var i = 0; i < pronoun.length; i++) {
+      console.log("the first loop");
+      for (var j = 0; j < adj.length; j++) {
+        console.log("the second loop");
+        for (var r = 0; r < noun.length; r++) {
+          console.log("the third loop");
+          console.log(pronoun[i] + adj[j] + noun[r] + ".com");
+        }
+      }
+    }
+    console.log("Hello Rigo from the console!");
+  };
+  //this one is only to see see a different way to do the code
+  function domainGenerator(pronoun, adj, noun) {
+    let result;
+    for (var i = 0; i < pronoun.length; i++) {
+      for (var j = 0; j < adj.length; j++) {
+        for (var r = 0; r < noun.length; r++) {
+          return (result = pronoun[i] + adj[j] + noun[r] + ".com");
+        }
+      }
+    }
+  }
 };
